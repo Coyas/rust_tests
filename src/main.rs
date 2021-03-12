f532689217261ea0e02d3e9f9105b0f72c85b734
Ailton Duarte <adidas.coyas@gmail.com>
2021-03-12 20:43:10 -0100

    vars

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+
 fn main() {
     // cargo é awesome
-    let x = 5;
+    /**
+     * signed: i8, i16, i32, i64, i128
+     * unsigned: u8, u16, u32, u64, u128
+     * architecture dependent: isize, usize
+     * default: i32 (+/- 2Billion)
+     * 
+     * Floats
+     * f32, f64
+     * default is f64
+     * 
+     * char
+     * 4 bits
+     */
+
+    let  x: i8 = 2;
     println!("valor de x: {}", x);
+    let sum = x + 120;
+    println!("valor de x: {}", sum);
+
+    let num: i128 = 10;
+    println!("num is {}", num);
+    println!("Min i128 is {}", std::i128::MIN);
+    println!("Max i128 is {}", std::i128::MAX);
+
+    let data: f32 = 10.2154;
+    println!("data is {}", data);
+    println!("f32 max lengh is {}", std::f32::MAX);
+    println!("f32 min lengh is {}", std::f32::MIN);
+
+    let car: char = 'a';
+    println!("is data: {}", car);
 }
