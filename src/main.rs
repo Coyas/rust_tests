ada8f77a61a780493766e26366e1f3d693a6ea95
Ailton Duarte <adidas.coyas@gmail.com>
2021-03-12 21:10:13 -0100

    strings é chars

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,53 @@
-
 fn main() {
-    // cargo é awesome
-    /**
-     * signed: i8, i16, i32, i64, i128
-     * unsigned: u8, u16, u32, u64, u128
-     * architecture dependent: isize, usize
-     * default: i32 (+/- 2Billion)
-     * 
-     * Floats
-     * f32, f64
-     * default is f64
-     * 
-     * char
-     * 4 bits
-     */
+    // string slice
+    let example_str: &str = "terra";
 
-    let  x: i8 = 2;
-    println!("valor de x: {}", x);
-    let sum = x + 120;
-    println!("valor de x: {}", sum);
+    let example_string: String = String::from("terrasystem");
 
-    let num: i128 = 10;
-    println!("num is {}", num);
-    println!("Min i128 is {}", std::i128::MIN);
-    println!("Max i128 is {}", std::i128::MAX);
+    println!("{}", example_str);
+    println!("{}", example_string);
 
-    let data: f32 = 10.2154;
-    println!("data is {}", data);
-    println!("f32 max lengh is {}", std::f32::MAX);
-    println!("f32 min lengh is {}", std::f32::MIN);
+    //translate between string and string slice
+    let string_from_str: String = example_str.to_string();
+    let string_from_str2: String = "some hardecoded".to_string();
 
-    let car: char = 'a';
-    println!("is data: {}", car);
+    println!("{}", string_from_str);
+    println!("{}", string_from_str2);
+
+    let string_from_hardecoded = String::from("some hardcoded");
+    let string_from_str_var = String::from(example_str);
+
+    println!("{}", string_from_hardecoded);
+    println!("{}", string_from_str_var);
+
+    let str_from_string: &str = &example_string;
+
+    println!("{}", str_from_string);
+
+    //concatenate 2 string
+    let combine_string_literals = ["first", "second"].concat();
+    let combine_with_format_macro = format!("{} {}", "first", "second");
+
+    println!("{}", combine_string_literals);
+    println!("{}", combine_with_format_macro);
+
+    let string_plus_str = example_string + example_str;
+
+    println!("{}", string_plus_str);
+
+    let mut mut_string = String::new();
+    mut_string.push_str(example_str);
+    mut_string.push_str("example_str");
+    mut_string.push('c');
+
+    let a = String::from("a");
+    let b = String::from("b");
+    let combine = a + &b + &mut_string;
+
+    println!("{}",combine);
+
+    // let char_by_index = &example_str.chars().nth(1);
+
+    // println!("{}", char_by_index); //errro
+
 }
